64da2bcaed2d04b93b833aa0e27f33ffea4e3e40
cnahian4@gmail.com <cnahian4@gmail.com>
2022-03-15 10:12:34 +0600

    Add project files.

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmpDbContext _db;
+        public EmployeeController(EmpDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            var employees = _db.Employees.ToList();
+
+            return Ok(employees);
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetEmployeeById(int id)
+        {
+            var employee = _db.Employees.FirstOrDefault(x => x.Id == id);
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ WebApi/Data/EmpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext(DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
